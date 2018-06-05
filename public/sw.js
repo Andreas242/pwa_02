5373e9e703abf6a9c8a08f14ba73a009c0fa7e69
AHLGREN Andreas <aahlgren@one.steria.dom>
2018-06-05 18:37:16 +0200

    added everything

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,23 @@
-self.addEventListener('install', function(event) {
-    console.log('[service worker] installing sw', event );
-})
+/* add serviceworker code here 
+   A servcieworker only listens to events and is refered to as 'self' 
+   ie. self.addEventListener('eventname', (event) =>) {
+
+   }
+
+   you need to *install* the service worker, *activate* it and probably see that you can intercept *fetch*-requests
+*/
+
+self.addEventListener('install', (event) => {
+    console.log('SW registering ');
+});
 
 self.addEventListener('activate', function(event) {
-    console.log('[service worker] activating sw', event);
+    console.log('[Service Worker] Activting Service Worker ...');
     return self.clients.claim();
-})
+  });
+  
 
-self.addEventListener('fetch', function(event) {
-    console.log('[service worker] fetching something', event );
+self.addEventListener('fetch', (event) => {
+    console.log('SW fetching', event);
     event.respondWith(fetch(event.request));
-})
-
+});
